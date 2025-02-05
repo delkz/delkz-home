cef44821c816cb5a0883fb8c9dcd2857224e243c
David Silva <davidlsilva.sp@gmail.com>
2025-02-05 09:56:40 -0300

    feat: added support to portuguese

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,10 +1,18 @@
 "use client"
 
+import Image from "next/image";
 import Link from "next/link";
-
 
 const Header = () => {
 
+    const changeLocale = (locale: string) => {
+        document.cookie = `locale=${locale}; path=/`;
+        window.location.reload();
+    };
+
+    const changeLocaleToPT = () => changeLocale('pt');
+    const changeLocaleToEn = () => changeLocale('en');
+    
     return (
         <div className="header">
             <div className="container mx-auto px-4">
@@ -14,8 +22,11 @@ const Header = () => {
                             <Link href={"/"}>Delkz</Link>
                         </h1>
                     </div>
-                    <div className="hidden">
-                        <button>
+                    <div>
+
+                        <button onClick={changeLocaleToPT}><Image width={32} height={32} src="https://flagsapi.com/BR/flat/32.png" alt="Portugues" /></button>
+                        <button onClick={changeLocaleToEn}><Image width={32} height={32} src="https://flagsapi.com/GB/flat/32.png" alt="English" /></button>
+                        <button className="hidden">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 5.25h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5" />
                             </svg>
--- next.config.ts
+++ next.config.ts
@@ -2,5 +2,9 @@ import createNextIntlPlugin from 'next-intl/plugin';
 
 const withNextIntl = createNextIntlPlugin('./app/i18n/requests.ts');
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        domains: ['flagsapi.com'],
+    },
+};
 export default withNextIntl(nextConfig);
