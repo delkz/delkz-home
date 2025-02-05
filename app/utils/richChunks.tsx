25fd3b03aa685cd3eb3932a644448ff1703c3582
David Silva <davidlsilva.sp@gmail.com>
2025-02-05 09:34:13 -0300

    feat: removed file richChunks.tsx

--- app/utils/richChunks.tsx
+++
@@ -1,9 +0,0 @@
-export const richChunks = {
-  p: (chunks) => <p>{chunks}</p>,
-  h2: (chunks) => <h2>{chunks}</h2>,
-  strong: (chunks) => <strong>{chunks}</strong>,
-  ul: (chunks) => <ul>{chunks}</ul>,
-  li: (chunks) => <li>{chunks}</li>,
-  section: (chunks) => <section>{chunks}</section>,
-  a: (chunks, attrs) => <a {...attrs}>{chunks}</a>
-}
