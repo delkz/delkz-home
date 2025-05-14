da203d19d2cfa2a94298f202b541251fb7699b9b
David Silva <davidlsilva.sp@gmail.com>
2025-05-14 11:03:53 -0300

    feat: reworks the companyproject tag

--- app/components/portfolio-card-tag.tsx
+++ app/components/portfolio-card-tag.tsx
@@ -17,19 +17,29 @@ const PortfolioCardTag = ({ tags }: PortfolioCardTagsProps) => {
                     case "wordpress":
                         tagBackground = "bg-blue-500";
                         break;
-                    case "external":
+                    case "companyproject":
                         tagBackground = "bg-purple-500";
                         break;
                     case "shopify":
                         tagBackground = "bg-green-500";
+                        break;
+                    case "serie a":
+                        tagBackground = "bg-black";
                         break;
                     default:
                         break;
                 }
 
 
+                let linkTag = "/terms-of-use#tags";
+                let isExternalLink = false;
+                if(tag === "Serie A"){
+                    isExternalLink = true;
+                    linkTag = "https://www.seriedesign.com.br/";
+                }
+
                 return (
-                    <Link href={"/terms-of-use#tags"} key={index} className={"text-[11px] font-semibold text-white rounded-full px-3 py-1 uppercase " + tagBackground}>{tag}</Link>
+                    <Link title={tag} href={linkTag} target={isExternalLink ? "_blank" : "_self"} key={index} className={"text-[11px] font-semibold text-white rounded-full px-3 py-1 uppercase " + tagBackground}>{tag}</Link>
                 )
             })}</>
     )
