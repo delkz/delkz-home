c05b66ef99b381c1b122824c7b11db05f199e658
David Silva <davidlsilva.sp@gmail.com>
2025-06-01 17:35:57 -0300

    feat: add ScriptsHeader component for Google Tag Manager and JSON-LD

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import Header from "./components/header";
 import Footer from "./components/footer";
 import { NextIntlClientProvider } from "next-intl";
 import { getLocale, getMessages } from "next-intl/server";
-import Script from "next/script";
+import ScriptsHeader from "./components/scripts-header";
 
 const robotoMono = Roboto_Mono({
   variable: "--font-roboto-mono",
@@ -30,17 +30,7 @@ export default async function RootLayout({
     <html lang={locale}>
       <head>
 
-    <Script
-      id="google-tag-manager"
-      strategy="afterInteractive"
-      dangerouslySetInnerHTML={{
-        __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','${process.env.GTM || ''}');`,
-      }}
-    />
+        <ScriptsHeader/>
 
       </head>
       <body
